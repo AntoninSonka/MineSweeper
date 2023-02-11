e91c0c0b00fb7605f5eea6eaccc0b5aa62d506b4
AntoninSonka <119446752+AntoninSonka@users.noreply.github.com>
2023-02-11 15:36:49 +0100

    Update Render.h

--- Render.h
+++ Render.h
@@ -4,8 +4,8 @@
 #include <SFML/Graphics.hpp>
 #include <vector>
 
-#define HIGHT 1000.f	//výška obrazovky
-#define LENGHT 1000.f	//dýlka obrazovky
+#define HEIGHT 750.f	//výška obrazovky
+#define LENGHT 750.f	//dýlka obrazovky
 #define OUTLINE 5.f		//mezery okolo jednotlivejch buněk
 
 //!musí se počítat s tim, že k výšce a šířce se přičte ještě OUTLINE. To je kvůli 
@@ -15,10 +15,12 @@ class Render
 private:
 	sf::RenderWindow window;
 	//Počet buněk na jednom řádku
-	int line = 22;
+	int line = 16;
 	//Deklarace okna a eventů
 	sf::Event event;
-	int pocetMin = 99;
+	int pocetMin = 40;
+	bool isGameOver = false;
+	int howManyOpen = 0;
 public:
 	//Constructor
 	Render();	//rend je funkce která se volá main funkcí v source. před zavolání je potřeba deklarovat přesně cellPos a cell pomocí klásy Pole.h a Pole.cpp. viz main()
@@ -30,4 +32,5 @@ public:
 	std::vector<std::vector<sf::RectangleShape>> cell;	//Obsahuje informace o konkrétních buňkách. např. velikost, barva a potom i pozice
 	std::vector<std::vector<bool>> isMine;
 	std::vector<std::vector<bool>> isOpend;
+	std::vector<std::vector<bool>> flag;
 };
