e5f136b8ffe60a3f2038ac520cfe18d6ee59b5ce
AntoninSonka <119446752+AntoninSonka@users.noreply.github.com>
2023-02-15 10:32:48 +0100

    Update Pole.h

--- Pole.h
+++ Pole.h
@@ -6,22 +6,23 @@
 
 //viz Render.h
 
-#define HEIGHT 750.f
-#define LENGHT 750.f
-#define OUTLINE 5.f
-
 class Pole
 {
 private:
 	//viz Render.h
-	int line = 16;
 public:
+	int row = 22;
+	int column = 22;
+	int pocetMin = 99;
+	float HEIGHT = 1000.f;	//výška obrazovky
+	float LENGHT = 1000.f;	//dýlka obrazovky
+	float OUTLINE = 3.f;		//mezery okolo jednotlivejch bunìk
 	//tahle classa používá pouze constructor
 	Pole();
 	void setPos();
 	void setTiles();
-	//vektor s informací o pozici každý buňky. Pak se dává do Render.h skrz main()
+	//vektor s informací o pozici každý buòky. Pak se dává do Render.h skrz main()
 	std::vector<std::vector<sf::Vector2f>> talesPos;
-	//vektor s informacemo o každý buňce, jako barva, velikost, atd. to samý, co s tim prvnim
+	//vektor s informacemo o každý buòce, jako barva, velikost, atd. to samý, co s tim prvnim
 	std::vector<std::vector<sf::RectangleShape>> tales;
 };
